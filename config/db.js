2f746d9894f8491a2b58d4005e80bb3e1ed79ccb
Pritesh23-P <pritesh0525@gmail.com>
2026-03-08 20:04:19 +0530

    update

--- config/db.js
+++ config/db.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/bankingAPI");
+
+        await mongoose.connect(process.env.MONGO_URI);
+
         console.log("MongoDB Connected");
+
     } catch (error) {
-        console.log(error);
+
+        console.error(error);
         process.exit(1);
+
     }
 };
 
